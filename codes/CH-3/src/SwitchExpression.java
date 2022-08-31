0938af181c4b83427f308d0f05e8913a62849692
1255108389 <1255108389@qq.com>
2022-08-31 16:08:23 +0800

    add someMDs

---
+++ codes/CH-3/src/SwitchExpression.java
@@ -0,0 +1,21 @@
+public class SwitchExpression {
+    enum Season {SPRING, SUMMER, AUTUMN, WINTER}
+
+    public static void main(String[] args) {
+        Season season = Season.SPRING;
+        String seasonName = switch (season) {
+            case SPRING -> "春天";
+            case SUMMER -> "夏天";
+            case AUTUMN -> "秋天";
+            case WINTER -> "冬天";
+            //标签为枚举的话，可以省略default
+        };
+        System.out.println("The seasonName is " + seasonName);
+
+        String isSeason = switch (season){
+            case SPRING, SUMMER, AUTUMN, WINTER -> "是";
+            default -> "否";
+        };
+        System.out.println(isSeason + "季节");
+    }
+}
